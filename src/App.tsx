57ec6e699d913d235c58dd95187c034a5b8da734
Hopper2021 <calico2021@gmail.com>
2022-01-26 14:58:20 -0600

    Add styling and font import

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
 import React from 'react';
 import './App.css';
 
-let name: String;
-let age: number;
-let isStudent: boolean;
-let hobbies: string[];
-
-// function called printName, takes in an object called name which is a string, and return the type void.
-let printName: (name: String) => void;
-
-//This is called a twople, can container 1 number and 1 string
-let role:[number,string]
-role = [5,"Software Developer"]
-
-// printName("Jessica");
-
-/* type templates the types for variables
-When you have properties of those variables listed, they always have to be present
-If you add a ? after the variable like so: ( age?: number; ), it is now optional */
-
-// type Person = {
-//   name: string;
-//   age?: number;
-// }
-
-// let person: Person = {
-//   name: "Jessica",
-// }
-
-// let lotsOfPeople: Person[];
-
-let personName: unknown;
-
-function App() {
+function App: React.FC = () {
   return (
-    <div className="App">{name}</div>
+    <div className="App">
+      <span className="heading">Taskify</span>
+    </div>
   );
 }
 
