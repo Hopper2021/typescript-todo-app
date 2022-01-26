f52641a2c7773535730182d16318c6258b6d9d47
Hopper2021 <calico2021@gmail.com>
2022-01-26 09:40:21 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: String;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+
+// function called printName, takes in an object called name which is a string, and return the type void.
+let printName: (name: String) => void;
+
+//This is called a twople, can container 1 number and 1 string
+let role:[number,string]
+role = [5,"Software Developer"]
+
+// printName("Jessica");
+
+/* type templates the types for variables
+When you have properties of those variables listed, they always have to be present
+If you add a ? after the variable like so: ( age?: number; ), it is now optional */
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: "Jessica",
+// }
+
+// let lotsOfPeople: Person[];
+
+let personName: unknown;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{name}</div>
   );
 }
 
