72a78eb7fa2a26387d2e2cc0e7b9d30766139485
Hopper2021 <calico2021@gmail.com>
2022-01-27 14:51:07 -0600

    Add logic for handleing todo list item

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './components/model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  // import the interface and have the empty array after the type listed below
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // event for handleAdd needs to be defined here and also in the Props in its component (InputField)
+  const handleAdd = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // if todo exists (which is the Inputs id) then set Todos and set Todo
+    if (todo) {
+      setTodos([...todos, { 
+        id: Date.now(), 
+        todo: todo, 
+        isDone: false 
+      } 
+    ]);
+      setTodo("");
+    }
+  }
 
   console.log(todo);
-  
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
